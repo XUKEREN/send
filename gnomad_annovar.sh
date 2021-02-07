43ffdacc37dbeaffe00127ab662226e5db7faae6
Keren Xu <xukeren418@gmail.com>
2021-02-07 15:30:30 -0800

    Update gnomad_annovar.sh

--- gnomad_annovar.sh
+++ gnomad_annovar.sh
@@ -4,3 +4,5 @@
 convert2annovar.pl -format vcf4 iv3gy3APiqTEz1aF.vcf > snplist.avinput  
 
 annotate_variation.pl -filter -dbtype gnomad30_genome -buildver hg38 ensembl/snplist.avinput .
+
+table_annovar.pl iv3gy3APiqTEz1aF.vcf /project/desmith_488/kerenxu/gnomad/humandb/ -buildver hg38 -out annotated -remove -protocol gnomad30_genome -operation f -vcfinput --dot2underline --polish --thread 20 --nastring .
