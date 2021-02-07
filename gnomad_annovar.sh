1a4f0d1612e66841c9e55e601cb32602f6383d4b
Keren Xu <xukeren418@gmail.com>
2021-02-07 15:22:01 -0800

    Update and rename gnomad_annovar to gnomad_annovar.sh

--- gnomad_annovar.sh
+++ gnomad_annovar.sh
@@ -1,3 +1,6 @@
+# send snp list to ensembl http://uswest.ensembl.org/Tools/VEP 
+# obtain vcf result 
+
 convert2annovar.pl -format vcf4 iv3gy3APiqTEz1aF.vcf > snplist.avinput  
 
 annotate_variation.pl -filter -dbtype gnomad30_genome -buildver hg38 ensembl/snplist.avinput .
